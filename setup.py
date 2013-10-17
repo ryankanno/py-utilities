f35f2f9bfbea78c68e76be36f270999cf9bae5ed
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-10-17 07:30:45 -0400

    Added setuptools based setup.py Renamed module from py-utilities to py_utilities

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(name='py-utilities',
+      version='0.0.1',
+      description='A set of Python utilities',
+      author='Ryan Kanno',
+      author_email='ryankanno@localkinegrinds.com',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      url="https://github.com/ryankanno/py-utilities"
+)
+
+# vim: filetype=python
