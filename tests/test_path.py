505fa08395b4a173c72734f78f79af958c0c96e3
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-12-26 08:22:22 -0500

    Added path test

---
+++ tests/test_path.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import ok_
+import os
+from py_utilities.path import expanded_abspath
+import unittest
+
+
+class TestPath(unittest.TestCase):
+
+    def test_expanded_abspath(self):
+        home = os.environ["HOME"]
+        ok_(expanded_abspath("~") == home)
+        ok_(expanded_abspath("~/foo") == os.path.join(home, 'foo'))
+
+# vim: filetype=python
