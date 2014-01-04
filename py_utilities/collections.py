be3e40e160bf653ce7af10dd60b7f5df40707df0
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-04 08:30:07 -0500

    Added the start of collections helper funcs

---
+++ py_utilities/collections.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import operator
+
+
+def index_min(seq):
+    """
+    Returns the index of the first occurrence minima in a sequence
+
+    >>> index_min([1,2,3,4,5,6,1,2,1])
+    0
+
+    >>> index_min([2,2,3,4,5,6,1,2,1])
+    6
+
+    >>> index_min([])
+    Traceback (most recent call last):
+        ...
+    ValueError: min() arg is an empty sequence
+
+    """
+    return min(enumerate(seq), key=operator.itemgetter(1))[0]
+
+
+def index_max(seq):
+    """
+    Returns the index of the first occurrence maxima in a sequence
+
+    >>> index_max([1,2,3,4,5,6,1,2,1])
+    5
+
+    >>> index_max([9,2,3,4,5,6,1,2,1])
+    0
+
+    >>> index_max([])
+    Traceback (most recent call last):
+        ...
+    ValueError: max() arg is an empty sequence
+
+    """
+    return max(enumerate(seq), key=operator.itemgetter(1))[0]
+
+
+def value_min(seq):
+    """
+    Returns the value of the first occurrence minima in a sequence
+
+    >>> value_min([1,2,3,4,5,6,1,2,1])
+    1
+
+    >>> value_min([2,2,3,4,5,6,8,2,9])
+    2
+
+    >>> value_min([])
+    Traceback (most recent call last):
+        ...
+    ValueError: min() arg is an empty sequence
+
+    """
+    return min(enumerate(seq), key=operator.itemgetter(1))[1]
+
+
+def value_max(seq):
+    """
+    Returns the value of the first occurrence maxima in a sequence
+
+    >>> value_max([1,2,3,4,5,6,1,2,1])
+    6
+
+    >>> value_max([2,2,3,4,5,6,8,2,9])
+    9
+
+    >>> value_max([])
+    Traceback (most recent call last):
+        ...
+    ValueError: max() arg is an empty sequence
+
+    """
+    return max(enumerate(seq), key=operator.itemgetter(1))[1]
+
+# vim: filetype=python
