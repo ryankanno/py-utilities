126d3bc8564776907000ea9a181b2ef839f60c57
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-04 08:23:03 -0500

    Added cd contextmanager Cleaned up /tmp in test cases

--- py_utilities/fs.py
+++ py_utilities/fs.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from contextlib import contextmanager
 import errno
 import os
+
+
+@contextmanager
+def cd(path=None):
+    prev_cwd = os.getcwd()
+    if path is not None:
+        os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
 
 
 def mkdir_p(path):
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from nose.tools import eq_
 from nose.tools import ok_
 from nose.tools import raises
 import os
+from py_utilities.fs import cd
 from py_utilities.fs import mkdir_p
 from py_utilities.fs import touch
 import shutil
+import tempfile
 import unittest
 
 
@@ -14,27 +17,45 @@ class TestFs(unittest.TestCase):
 
     def setUp(self):
         self.cwd = os.path.dirname(os.path.realpath(__file__))
+        self.temp_dir = tempfile.gettempdir()
 
     def tearDown(self):
-        if os.path.exists('/tmp/foo'):
-            shutil.rmtree('/tmp/foo')
+        tmp_foo = os.path.join(self.temp_dir, 'foo')
+        if os.path.exists(tmp_foo):
+            shutil.rmtree(tmp_foo)
+
+    def test_cd(self):
+        curr = os.getcwd()
+        path = os.path.realpath(self.temp_dir)
+        with cd(path):
+            cwd = os.getcwd()
+            ok_(curr != cwd)
+            eq_(cwd, path)
+        cwd = os.getcwd()
+        eq_(curr, cwd)
 
     def test_mkdir_p(self):
-        mkdir_p('/tmp/foo/bar/rules')
-        ok_(os.path.exists('/tmp/foo'))
-        ok_(os.path.exists('/tmp/foo/bar'))
-        ok_(os.path.exists('/tmp/foo/bar/rules'))
+        tmp_foo = os.path.join(self.temp_dir, 'foo')
+        tmp_foo_bar = os.path.join(self.temp_dir, 'foo', 'bar')
+        tmp_foo_bar_rules = os.path.join(self.temp_dir, 'foo', 'bar', 'rules')
+        mkdir_p(tmp_foo_bar_rules)
+        ok_(os.path.exists(tmp_foo))
+        ok_(os.path.exists(tmp_foo_bar))
+        ok_(os.path.exists(tmp_foo_bar_rules))
 
     @raises(OSError)
     def test_mkdir_p_raise_exception(self):
-        mkdir_p('/tmp/foo/bar/rules')
-        f = open('/tmp/foo/bar/rules/file', 'w')
+        tmp_foo_bar_rules = os.path.join(self.temp_dir, 'foo', 'bar', 'rules')
+        mkdir_p(tmp_foo_bar_rules)
+        tmp_file = os.path.join(tmp_foo_bar_rules, 'file')
+        f = open(tmp_file, 'w')
         f.close()
-        mkdir_p('/tmp/foo/bar/rules/file')
+        mkdir_p(tmp_file)
 
     def test_mkdir_p_should_not_raise_exception(self):
-        mkdir_p('/tmp/foo/bar/rules')
-        mkdir_p('/tmp/foo/bar/rules')
+        tmp_foo_bar_rules = os.path.join(self.temp_dir, 'foo', 'bar', 'rules')
+        mkdir_p(tmp_foo_bar_rules)
+        mkdir_p(tmp_foo_bar_rules)
 
     def test_touch(self):
         file = os.path.join(self.cwd, 'test_fs.py')
