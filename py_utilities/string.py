90ac68c04db122d3b13f8d08b2095d7e3d76fce1
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-09 08:09:29 -0500

    Moved testing back to 2.7 -> will need to run through py2to3 Added string utilities

---
+++ py_utilities/string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def truncate(str, length, trailing_chars='...'):
+    """
+    Returns a truncated str of length with a trailing '...'
+
+    >>> truncate('foobar', 3)
+    'foo...'
+
+    >>> truncate('foobar', 1)
+    'f...'
+
+    >>> truncate('', 1)
+    ''
+
+    >>> truncate('hello', 100)
+    'hello'
+
+    >>> print truncate(u"今日は", 1)
+    '今...'
+    """
+    return (str[:length] + trailing_chars) if len(str) > length else str
+
+# vim: filetype=python
