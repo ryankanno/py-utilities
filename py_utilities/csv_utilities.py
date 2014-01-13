9e96c6a266b5ffef75bd3b50d30fd0cba8e7f1ff
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-13 07:06:50 -0500

    Added list_to_csv

--- py_utilities/csv_utilities.py
+++ py_utilities/csv_utilities.py
@@ -12,6 +12,15 @@ def csv_to_headers(file_path, has_headers=None, delimiters=[',', ';']):
     return _csv_to_headers_rows_tuple(file_path, has_headers, delimiters)[0]
 
 
+def list_to_csv(file_path, data_as_list, delimiter=',', quoting=csv.QUOTE_ALL):
+    """
+    Writes data_as_list into a csv file at file_path
+    """
+    with open(file_path, 'wb') as f:
+        writer = csv.writer(f, delimiter=delimiter, quoting=quoting)
+        writer.writerows(data_as_list)
+
+
 def _csv_to_headers_rows_tuple(file_path,
                                has_headers=None,
                                delimiters=[',', ';']):
--- tests/test_csv.py
+++ tests/test_csv.py
@@ -5,6 +5,8 @@ from nose.tools import ok_
 import os
 from py_utilities.csv_utilities import csv_to_headers
 from py_utilities.csv_utilities import csv_to_list
+from py_utilities.csv_utilities import list_to_csv
+import tempfile
 import unittest
 
 
@@ -16,6 +18,7 @@ class TestCsv(unittest.TestCase):
                                              'test_csv_to_list_headers.csv')
         self.csv_no_headers_path = os.path.join(
             self.cwd, 'data', 'test_csv_to_list_no_headers.csv')
+        self.temp_dir = tempfile.gettempdir()
 
     def test_csv_to_list(self):
         data = csv_to_list(self.csv_headers_path)
@@ -28,5 +31,14 @@ class TestCsv(unittest.TestCase):
         ok_(len(data) == 2)
         data = csv_to_headers(self.csv_no_headers_path, has_headers=False)
         ok_(len(data) == 0)
+
+    def test_list_to_csv(self):
+        tmp_data = [['Ryan', '1'], ['Bob', '2'], ['Foo', '3']]
+        tmp_csv = os.path.join(self.temp_dir, 'csv_test')
+        list_to_csv(tmp_csv, tmp_data)
+        with open(tmp_csv) as f:
+            for i, l in enumerate(f):
+                pass
+        ok_(i + 1 == len(tmp_data))
 
 # vim: filetype=python
