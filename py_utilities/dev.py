7e46cf4fada5a51f5415b8e5d3825b4ed3f5b155
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-14 04:56:26 -0500

    Added deprecated decorator

--- py_utilities/dev.py
+++ py_utilities/dev.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import BaseHTTPServer
+import functools
+import warnings
 import webbrowser
 
 
@@ -13,5 +15,18 @@ def browser(html, ip='127.0.0.1', port=0):
     server = BaseHTTPServer.HTTPServer((ip, port), RequestHandler)
     webbrowser.open("http://{0}:{1}".format(*server.server_address))
     server.handle_request()
+
+
+def deprecated(message=""):
+    def decorator(deprecated_func):
+        msg = message or \
+            "Function '{}' is deprecated.".format(deprecated_func.__name__)
+
+        @functools.wraps(deprecated_func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+            return deprecated_func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 # vim: filetype=python
---
+++ tests/test_dev.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from py_utilities.dev import deprecated
+import unittest
+import warnings
+
+
+@deprecated()
+def test_add_no_msg():
+    return 1 + 1
+
+
+@deprecated("Custom message, yo!")
+def test_add_msg():
+    return 1 + 1
+
+
+class TestDev(unittest.TestCase):
+
+    def test_deprecated(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            test_add_no_msg()
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+            assert "Function 'test_add_no_msg' is deprecated." \
+                in str(w[-1].message)
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            test_add_msg()
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+            assert "Custom message, yo!" in str(w[-1].message)
+
+# vim: filetype=python
