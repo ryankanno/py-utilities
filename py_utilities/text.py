c67ec60e7105ef6f6655bf2793a9ce73a7b4955c
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-01-25 04:59:45 -0600

    Added strip to lines

--- py_utilities/text.py
+++ py_utilities/text.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
-def lines(file_path):
+def lines(file_path, strip=True):
     """
     Returns a list of lines from file_path
     """
     with open(file_path, 'r') as f:
-        return [line for line in f]
+        return [strip_if_true(line, strip) for line in f]
+
+
+def strip_if_true(text, strip):
+    """
+    Strips text if strip is True
+    """
+    return text.strip() if strip else text
 
 # vim: filetype=python
--- tests/test_text.py
+++ tests/test_text.py
@@ -4,6 +4,7 @@
 from nose.tools import ok_
 import os
 from py_utilities.text import lines
+from py_utilities.text import strip_if_true
 import unittest
 
 
@@ -15,8 +16,22 @@ class TestText(unittest.TestCase):
                                              'test_csv_to_list_headers.csv')
 
     def test_lines(self):
-        list_of_lines = lines(self.csv_headers_path)
+        list_of_lines = lines(self.csv_headers_path, strip=False)
         ok_(len(list_of_lines) == 4)
         ok_(list_of_lines[0] == "Name,Age\n")
+        list_of_lines = lines(self.csv_headers_path, strip=True)
+        ok_(len(list_of_lines) == 4)
+        ok_(list_of_lines[0] == "Name,Age")
+        list_of_lines = lines(self.csv_headers_path)
+        ok_(list_of_lines[0] == "Name,Age")
+
+    def test_strip_if_true(self):
+        test_with_spaces = " T es t "
+        test_without_spaces = "T es t"
+        test_stripped = strip_if_true(test_with_spaces, True)
+        test_not_stripped = strip_if_true(test_with_spaces, False)
+        ok_(test_without_spaces == test_stripped)
+        ok_(test_with_spaces == test_not_stripped)
+
 
 # vim: filetype=python
