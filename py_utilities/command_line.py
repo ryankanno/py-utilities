085ebf43d0a72b1f9ac28e3ef1fa40f9fd08d54b
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-02-01 08:23:40 -0500

    Added prompt_yes_no

---
+++ py_utilities/command_line.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def prompt_yes_no(prompt_msg, default=None, num_retries=5):
+    """
+    Yes/no prompts a user with `prompt_msg`. Default can be `yes`, `no`, or
+    None. If user answer is not valid, he/she has `num_retries` to provide a
+    valid yes/no prompt.
+    """
+    def _get_prompt_defaults(default):
+        if default is None:
+            return "[y/n]"
+        elif default == "yes":
+            return "[Y/n]"
+        elif default == "no":
+            return "[y/N]"
+        else:
+            raise ValueError("Invalid default value: '%s'" % default)
+
+    prompt_msg = "{0} {1} ".format(prompt_msg, _get_prompt_defaults(default))
+
+    while True:
+        choice = raw_input(prompt_msg).lower()
+
+        if default is not None and choice == '':
+            choice = default
+        if choice in ('y', 'ye', 'yes', '1'):
+            return True
+        if choice in ('n', 'no', '0'):
+            return False
+
+        num_retries = num_retries - 1
+        if num_retries < 0:
+            raise IOError
+
+        sys.stdout.write("Please respond with a valid 'yes' or 'no'\n")
+
+# vim: filetype=python
