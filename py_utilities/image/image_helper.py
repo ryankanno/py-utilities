87c034f323d1fd97872a3fe0cfd38dd17831ad41
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-03-11 08:56:19 -0400

    Added image_helper (size object)

---
+++ py_utilities/image/image_helper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Size(object):
+    def __init__(self, size_tuple):
+        self.width = float(size_tuple[0])
+        self.height = float(size_tuple[1])
+
+    @property
+    def aspect_ratio(self):
+        return self.width / self.height
+
+    @property
+    def size(self):
+        return (self.width, self.height)
+
+
+
+
+
+# vim: filetype=python
