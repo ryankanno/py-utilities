c2c838793930c745a0f337bdc7851e7fef3b3382
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-04-01 08:04:47 -0400

    Added first_dir_path func

--- py_utilities/fs/path_utilities.py
+++ py_utilities/fs/path_utilities.py
@@ -15,4 +15,13 @@ def filename(path):
     dirs, f = ntpath.split(path)
     return f or ntpath.basename(dirs)
 
+
+def get_first_dir_path(dir_paths):
+    """
+    Returns the first valid dir path in dir_paths
+    """
+    dir_paths = [path for path in dir_paths if os.path.isdir(path)]
+    return dir_paths[0] if len(dir_paths) > 0 else None
+
+
 # vim: filetype=python
--- tests/fs/test_path_utilities.py
+++ tests/fs/test_path_utilities.py
@@ -5,6 +5,8 @@ from nose.tools import ok_
 import os
 from py_utilities.fs.path_utilities import expanded_abspath
 from py_utilities.fs.path_utilities import filename
+from py_utilities.fs.path_utilities import get_first_dir_path
+import tempfile
 import unittest
 
 
@@ -22,5 +24,16 @@ class TestPath(unittest.TestCase):
                  '\\foo\\bar\\', '\\foo\\bar', 'foo\\bar\\', 'foo\\bar']
         for path in paths:
             ok_(filename(path) == 'bar')
+
+    def test_get_first_dir_path(self):
+        dir = tempfile.mkdtemp()
+        home = os.environ["HOME"]
+        fake = '/foo/bar/x/y/z/a'
+        ok_(dir == get_first_dir_path([dir]))
+        ok_(dir == get_first_dir_path([dir, home]))
+        ok_(home == get_first_dir_path([home, dir]))
+        ok_(home == get_first_dir_path([fake, home, dir]))
+        ok_(dir == get_first_dir_path([fake, dir, home]))
+
 
 # vim: filetype=python
