f730dc0c760b9750d1c4b1e2c6e97e0f138a38fb
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-04-02 07:59:02 -0400

    Added is_leap_year, random_datetime

--- py_utilities/time/date_utilities.py
+++ py_utilities/time/date_utilities.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import calendar
+from datetime import datetime
+import random
+
+
+def is_leap_year(year):
+    """
+    Returns true if the year is a leap year, false otherwise
+
+    >>> is_leap_year(2008)
+    True
+    >>> is_leap_year(2012)
+    True
+    >>> is_leap_year(2013)
+    False
+    """
+    return calendar.isleap(year)
+
 
 def is_weekday(date):
     """
     Returns true if date is a weekday, false otherwise
 
-    >>> import datetime; from datetime import timedelta;
-    >>> date = datetime.datetime.now()
+    >>> from datetime import timedelta;
+    >>> date = datetime.now()
     >>> last_friday = date - timedelta(days=date.weekday()) \
     + timedelta(days=4, weeks=-1)
     >>> is_weekday(last_friday)
@@ -20,8 +38,8 @@ def is_weekend(date):
     """
     Returns true if date is a weekend, false otherwise
 
-    >>> import datetime; from datetime import timedelta;
-    >>> date = datetime.datetime.now()
+    >>> from datetime import timedelta;
+    >>> date = datetime.now()
     >>> last_friday = date - timedelta(days=date.weekday()) \
     + timedelta(days=4, weeks=-1)
     >>> is_weekend(last_friday)
@@ -29,5 +47,14 @@ def is_weekend(date):
     """
     return date.isoweekday() > 5
 
+
+def random_datetime(start_timestamp, end_timestamp, tz=None):
+    """
+    Returns a random datetime between two timestamps and converts to the tz if
+    provided
+    """
+    interval = end_timestamp - start_timestamp
+    random_timestamp = start_timestamp + (random.random() * interval)
+    return datetime.fromtimestamp(random_timestamp, tz)
 
 # vim: filetype=python
