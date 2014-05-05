98020798c5247520ec93dbf6c347d9d0a0a7541d
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-05 07:36:44 -0400

    Added equality mixin

---
+++ py_utilities/mixins/equality.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class EqualityMixin(object):
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and
+                self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+# vim: filetype=python
