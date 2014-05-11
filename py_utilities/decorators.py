aa6c8f75291eb9727af459137bfd69ad5cd29a7e
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-11 04:41:29 -0400

    Added run_once decorator

--- py_utilities/decorators.py
+++ py_utilities/decorators.py
@@ -7,8 +7,19 @@ import functools
 def apply(func_to_apply):
     def decorator(func):
         @functools.wraps(func)
-        def f(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             return func_to_apply(func(*args, **kwargs))
-        return f
+        return wrapper
     return decorator
+
+
+def run_once(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return func(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
 # vim: filetype=python
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,6 +3,7 @@
 
 from nose.tools import ok_
 from py_utilities.decorators import apply
+from py_utilities.decorators import run_once
 import unittest
 
 
@@ -12,6 +13,13 @@ def add_1(iterable):
 
 @apply(add_1)
 def foo():
+    """ Foo docstring """
+    return [1, 2, 3]
+
+
+@run_once
+def foo_once():
+    """ Foo_once docstring """
     return [1, 2, 3]
 
 
@@ -20,5 +28,15 @@ class TestDecorator(unittest.TestCase):
     def test_apply(self):
         result = foo()
         ok_(result == [2, 3, 4])
+        ok_(foo.__name__ == 'foo')
+        ok_(foo.__doc__ == ' Foo docstring ')
+
+    def test_run_once(self):
+        result = foo_once()
+        ok_(result == [1, 2, 3])
+        result2 = foo_once()
+        ok_(result2 is None)
+        ok_(foo_once.__name__ == 'foo_once')
+        ok_(foo_once.__doc__ == ' Foo_once docstring ')
 
 # vim: filetype=python
