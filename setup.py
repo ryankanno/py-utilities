4c5d8214f3afe517b5daf9e44290ce27a96fe1e4
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-17 06:59:23 -0400

    Preparing for pypi

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='py-utilities',
-      version='0.0.1',
-      description='A set of Python utilities',
-      author='Ryan Kanno',
-      author_email='ryankanno@localkinegrinds.com',
-      packages=find_packages(),
-      url="https://github.com/ryankanno/py-utilities",
-      install_requires=['pytz', 'importlib', 'scrypt'],
-      license='MIT',
-      tests_require=['flake8', 'mock', 'nose', 'nosexcover'])
+setup(
+    name='py-utilities',
+    version='0.0.1',
+    author='Ryan Kanno',
+    author_email='ryankanno@localkinegrinds.com',
+    url="https://github.com/ryankanno/py-utilities",
+    packages=find_packages(),
+    install_requires=['pytz', 'importlib', 'scrypt'],
+    license='MIT',
+    tests_require=['flake8', 'mock', 'nose', 'nosexcover'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities'
+    ],
+)
 
 # vim: filetype=python
